56831a43c9b17e23a94b37f84b23b202c80a8200
Sameer Raj <sameer.raj1807@gmail.com>
2026-06-24 23:23:04 +0530

    Update Pandas_Practice.py

--- Week2/Pandas_Practice.py
+++ Week2/Pandas_Practice.py
@@ -1,9 +1,12 @@
 Cell 1
 # Pandas Practice
 
+----------------------------------------
 Learning data handling and analysis using Pandas.
 Cell 2
 import pandas as pd
+
+----------------------------------------
 Cell 3
 data = {
     "Product_ID": [101, 102, 103, 104, 105],
@@ -14,38 +17,60 @@ data = {
 df = pd.DataFrame(data)
 
 df
+
+----------------------------------------
 Cell 4
 print(df.head())
+
+----------------------------------------
 Cell 5
 print(df.shape)
+
+----------------------------------------
 Cell 6
 print(df.columns)
+
+----------------------------------------
 Cell 7
 print(df["Defects"])
+
+----------------------------------------
 Cell 8
 average_defects = df["Defects"].mean()
 
 print("Average Defects:", average_defects)
+
+----------------------------------------
 Cell 9
 max_defects = df["Defects"].max()
 
 print("Maximum Defects:", max_defects)
+
+----------------------------------------
 Cell 10
 filtered_products = df[df["Defects"] > 3]
 
 filtered_products
+
+----------------------------------------
 Cell 11
 df["Inspection_Status"] = df["Defects"].apply(
     lambda x: "Fail" if x > 5 else "Pass"
 )
 
 df
+
+----------------------------------------
 Cell 12
 status_count = df["Inspection_Status"].value_counts()
 
 print(status_count)
+
+----------------------------------------
 Cell 13
 df.describe()
+
+----------------------------------------
 Cell 14
 total_products = len(df)
 
